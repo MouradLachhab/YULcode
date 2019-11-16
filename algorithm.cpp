4526bebc3f9a03fdfcc49039233728a7f9473e34
Sevla <dasilvaalvesb@gmail.com>
2019-11-16 14:00:06 -0500

    update

---
+++ algorithm.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include "point.h"
+#include "case.h"
+#include <set>
+
+vector<Point> algo (Point start, Point end, int sizeTab) {
+	int n = sizeTab;
+	int m = sizeTab;
+
+	Case* etage = new Case[n][m];
+
+	set<Case> arbre;
+
+}
